9ae73bf5cd6945d30e25d6781a31479141e8760b
Joelios <joel.mesmer@libracore.com>
2017-10-20 16:00:42 +0200

    Added filter for quotation

--- firetesting/fire_testing/doctype/crono/crono.js
+++ firetesting/fire_testing/doctype/crono/crono.js
@@ -3,6 +3,12 @@
 
 frappe.ui.form.on('Crono', {
 	refresh: function(frm) {
-
+		cur_frm.fields_dict['quotation_number'].get_query = function(doc) {
+			return {
+				filters: {
+					"customer": frm.doc.customer
+				}
+			}
+		}
 	}
 });
