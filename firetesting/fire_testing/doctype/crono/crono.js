38579dc9255c2ecd6d917b7b6d153bf8bffe09e9
Joelios <joel.mesmer@libracore.com>
2017-10-22 23:19:46 +0200

    added some comments

--- firetesting/fire_testing/doctype/crono/crono.js
+++ firetesting/fire_testing/doctype/crono/crono.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on('Crono', {
 	refresh: function(frm) {
+		//Filter for quotation_number based on customer link field
 		cur_frm.fields_dict['quotation_number'].get_query = function(doc) {
 			return {
 				filters: {
@@ -10,6 +11,7 @@ frappe.ui.form.on('Crono', {
 				}
 			}
 		}
+		//Filter for sales_order based on customer link field
 		cur_frm.fields_dict['sales_order'].get_query = function(doc) {
 			return {
 				filters: {
@@ -17,36 +19,40 @@ frappe.ui.form.on('Crono', {
 				}
 			}
 		}
-		
+		//Fetch value transaction_date into field quotation_exit from quotation based quotation_number
 		cur_frm.add_fetch('quotation_number','transaction_date','quotation_exit');
 		
 	},
 	
 	crono_status: function(frm) {
+		//Work i progress only when material received
 		if(!cur_frm.doc.material_receive && cur_frm.doc.crono_status=='Work in progress') {
 			cur_frm.set_value('crono_status', '');
 			frappe.msgprint('For status work in progress, the material must be available!');
 		}
+		//Finish only when material received
 		if(!cur_frm.doc.material_receive && cur_frm.doc.crono_status=='Finish') {
 			cur_frm.set_value('crono_status', '');
 			frappe.msgprint('For status Finish, the material must be available!');
 		}
+		//Check if a quotation is linked 
 		if(!cur_frm.doc.quotation_exit && cur_frm.doc.crono_status!='') {
 			cur_frm.set_value('crono_status', '');
 			frappe.msgprint('At first, a quotation must be done!');
 		}
+		//Check if a sales order is linked
 		if(!cur_frm.doc.sales_order && cur_frm.doc.crono_status!='' && cur_frm.doc.crono_status!='Backlog') {
 			cur_frm.set_value('crono_status', '');
 			frappe.msgprint('At first, a sales order must exist!');
 		}
 	},
-	
+	//Set prop. "set_only_once" of sales order
 	sales_order: function(frm) {
 		if(cur_frm.doc.sales_order!='') {
 			cur_frm.set_df_property('sales_order','set_only_once',1);
 		}
 	},
-	
+	//Set/remove mandatory from material based on material receive date
 	material_receive: function(frm) {
 		if(!cur_frm.doc.material_receive) {
 			cur_frm.set_df_property('material','reqd',0);
