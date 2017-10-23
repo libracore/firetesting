e5b6e633c5137fbc93eede071666ff6320a4032a
Joelios <joel.mesmer@libracore.com>
2017-10-23 08:40:43 +0200

    small corrections

--- firetesting/fire_testing/doctype/crono/crono.js
+++ firetesting/fire_testing/doctype/crono/crono.js
@@ -46,10 +46,16 @@ frappe.ui.form.on('Crono', {
 			frappe.msgprint('At first, a sales order must exist!');
 		}
 	},
-	//Set prop. "set_only_once" of sales order
-	sales_order: function(frm) {
-		if(cur_frm.doc.sales_order!='') {
+	//Set prop. "set_only_once" after save
+	validate: function(frm) {
+		if(cur_frm.doc.sales_order) {
 			cur_frm.set_df_property('sales_order','set_only_once',1);
+		}
+		if(cur_frm.doc.red_standard_selection) {
+			cur_frm.set_df_property('red_standard_selection','set_only_once',1);
+		}
+		if(cur_frm.doc.material_receive) {
+			cur_frm.set_df_property('material_receive','set_only_once',1);
 		}
 	},
 	//Set/remove mandatory from material based on material receive date
