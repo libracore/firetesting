a17d3faec06d73afe1288f5026e0d1c9e7643b74
lasalesi <lars.mueller@libracore.com>
2017-11-26 22:52:49 +0100

    content technical data sheet

--- firetesting/fire_testing/doctype/technical_data_sheet/technical_data_sheet.js
+++ firetesting/fire_testing/doctype/technical_data_sheet/technical_data_sheet.js
@@ -6,3 +6,22 @@ frappe.ui.form.on('Technical data sheet', {
 
 	}
 });
+
+// normal field change on crono does not work on the initial set
+function loadCustomer() {
+        cur_frm.add_fetch('crono', 'customer', 'customer');
+};
+
+// mutation observer for crono changes
+var cronoObserver = new MutationObserver(function(mutations) {
+     mutations.forEach(function(mutation) {
+        loadCustomer();
+     });
+});
+var target=document.querySelector('div[data-fieldname="crono"] .control-input-wrapper .control-value');
+var options = {
+     attributes: true,
+     characterData: true
+};
+cronoObserver.observe(target, options);
+
