c950b75d4010ea0c6cb5eef7e150d96919c22fb2
lasalesi <larsericmueller@gmail.com>
2018-04-17 23:28:24 +0200

    print format for verification

--- firetesting/fire_testing/doctype/crono_verification/crono_verification.js
+++ firetesting/fire_testing/doctype/crono_verification/crono_verification.js
@@ -10,7 +10,7 @@ frappe.ui.form.on('Crono Verification', {
                 // define title
                 var title = "Verification " + frm.doc.crono;
 
-                cur_frm.set_value("title", title);
+                cur_frm.set_value('title', title);
                 // get customer
                 get_customer(frm);
                 // read material
@@ -24,6 +24,25 @@ frappe.ui.form.on('Crono Verification', {
                 });
             }
         }
-
-	}
+	},
+    validate: function(frm) {
+        if (frm.doc.shape == "Circular") {
+            var diameter = frm.doc.declared_diameter;
+            var diameter_min = 0.9 * diameter;
+            var diameter_max= 1.1 * diameter;
+            frm.set_value('declared_diameter_min', diameter_min);
+            frm.set_value('declared_diameter_max', diameter_max);
+        } else {
+            var diameter1 = frm.doc.major_axis_diameter;
+            var diameter2 = frm.doc.minor_axis_diameter;
+            var diameter1_min = 0.9 * diameter1;
+            var diameter1_max= 1.1 * diameter1;
+            var diameter2_min = 0.9 * diameter2;
+            var diameter2_max= 1.1 * diameter2;
+            frm.set_value('major_axis_diameter_min', diameter1_min);
+            frm.set_value('major_axis_diameter_max', diameter1_max);
+            frm.set_value('minor_axis_diameter_min', diameter2_min);
+            frm.set_value('minor_axis_diameter_max', diameter2_max);
+        } 
+    }
 });
