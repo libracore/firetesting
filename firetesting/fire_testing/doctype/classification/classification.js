c5a2a45f7accf5ee7e234ea06f7a271157bd41ee
lasalesi <larsericmueller@gmail.com>
2018-05-03 13:43:13 +0200

    new classification creationg from crono (#696) and product name as small text (#695)

--- firetesting/fire_testing/doctype/classification/classification.js
+++ firetesting/fire_testing/doctype/classification/classification.js
@@ -11,8 +11,39 @@ frappe.ui.form.on('Classification', {
 				}
 			}
 		};
-	},
-	setup: function(frm) {
-		cur_frm.add_fetch('sales_order','customer','customer');
-	}
+        
+        // check if this is a new record
+        if (frm.doc.__islocal) {
+            var last_route = frappe.route_history.slice(-2, -1)[0];
+            // last_route is a array with ["Form", "Crono", "CRONO-00003"]
+            if (last_route[1] == "Crono") {
+               // coming from a Crono
+               get_info(frm, last_route[2]);
+            } else {
+                // coming from sales order: fetch customer from sales order
+                cur_frm.add_fetch('sales_order','customer','customer');
+            }
+        }
+        
+	} 
 });
+
+function get_info(frm, crono) {
+    frappe.call({
+        "method": "frappe.client.get",
+        "args": {
+            "doctype": "Crono",
+            "name": crono
+        },
+        "callback": function(response) {
+            var crono = response.message;
+
+            if (crono) {
+                frm.set_value('customer', crono.customer);
+                frm.set_value('sales_order', crono.sales_order);
+            } else {
+                frappe.msgprint("Crono not found");
+            }
+        }
+    });
+}
