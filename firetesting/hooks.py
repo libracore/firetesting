23495ec59d05b83ae21ab0d2e0cd382bd3b6d5ac
lasalesi <larsericmueller@gmail.com>
2019-08-27 11:29:29 +0200

    included customisation to core doctypes

--- firetesting/hooks.py
+++ firetesting/hooks.py
@@ -35,7 +35,9 @@ doctype_js = {
     "EN 61034 2" : "public/js/fire-common.js",
     "EN 60332 1 2" :  "public/js/fire-common.js",
     "EN 60754 2" :    "public/js/fire-common.js",
-    "Crono Verification" : "public/js/fire-common.js" }
+    "Crono Verification" : "public/js/fire-common.js",
+	"Sales Order": "public/js/sales_order.js"
+}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
